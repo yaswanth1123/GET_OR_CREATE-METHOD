a120e6d699847e44553f558ac3db64708dafe9f4
hanumanthk1128 <khanumanth.msbi@gmail.com>
2022-07-06 19:30:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from app.models import *
+
+def display_topics(request):
+    LOT=Topic.objects.all()
+    LOW=Webpages.objects.all()
+    d={'topics':LOT,'webpages':LOW}
+    return render(request,'display_topics.html',d)
+
+
--- projectyash14/urls.py
+++ projectyash14/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_topics/',display_topics,name='display_topics'),
 ]
